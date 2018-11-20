568e27f0aa15798f48e726f03bb5823beb9a5e6a
Victor Chen <yvictorck@gmail.com>
2018-11-20 04:01:01 -0800

    a

--- core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/ContainerProxy.scala
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/ContainerProxy.scala
@@ -137,8 +137,8 @@ class ContainerProxy(
         job.action.exec.image,
         job.action.exec.pull,
         (job.action.limits.memory.megabytes.MB),
-        poolConfig.cpuShare(job.action.limits.memory.megabytes.MB))
-      val meow = poolConfig.cpuShare(job.action.limits.memory.megabytes.MB)
+        poolConfig.cpuShare(job.action.limits.memory.megabytes.MB)*2)
+      val meow = poolConfig.cpuShare(job.action.limits.memory.megabytes.MB)*2
       logging.info(this, s"meow $meow")
 
       // container factory will either yield a new container ready to execute the action, or
