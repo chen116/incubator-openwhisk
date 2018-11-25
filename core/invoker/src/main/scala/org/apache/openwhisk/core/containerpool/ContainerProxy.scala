bf23593ebe4d1305ce9d61c5660a7dd7ed747312
Victor Chen <yvictorck@gmail.com>
2018-11-25 03:55:29 -0800

    a

--- core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/ContainerProxy.scala
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/ContainerProxy.scala
@@ -354,7 +354,7 @@ class ContainerProxy(
    */
   def initializeAndRun(container: Container, job: Run)(implicit tid: TransactionId): Future[WhiskActivation] = {
 
-    logging.info(this, s"meow id $container.id")
+    logging.info(this, s"meowid $container.id")
 
     val actionTimeout = job.action.limits.timeout.duration
 
