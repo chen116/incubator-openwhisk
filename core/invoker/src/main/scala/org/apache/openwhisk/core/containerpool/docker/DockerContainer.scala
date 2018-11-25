870896b45bea931c40d949adce4e997a38ee2373
Victor Chen <yvictorck@gmail.com>
2018-11-25 06:10:40 -0800

    a

--- core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/docker/DockerContainer.scala
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/docker/DockerContainer.scala
@@ -202,9 +202,8 @@ class DockerContainer(protected val id: ContainerId,
     super.destroy()
     docker.rm(id)
   }
-   override   def meow_id()(implicit transid: TransactionId): String = {
+   override   def meow_id()(implicit transid: TransactionId): Future[Unit] = {
     docker.meow_update(id)
-    return id.toString
   }
 
   /**
