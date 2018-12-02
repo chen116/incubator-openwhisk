17c74032f6096c2b2f95c300fba5005f4d599d44
Victor Chen <yvictorck@gmail.com>
2018-12-01 22:56:02 -0800

    a

--- core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/docker/DockerContainer.scala
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/docker/DockerContainer.scala
@@ -90,6 +90,7 @@ object DockerContainer {
       // meow_quo.toString,
       "--cpu-period",
       "1000000",
+      "--cpu-rt-runtime","500000",
       "--cpuset-cpus",
       "3",
       "--ulimit", "rtprio=99" ,"--cap-add","sys_nice" ,
