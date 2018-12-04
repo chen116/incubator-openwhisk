40fca4122a72d92521668b489dd9d712d4236312
Victor Chen <yvictorck@gmail.com>
2018-12-04 09:20:24 -0800

    a

--- tests/performance/gatling_tests/src/gatling/scala/org/apache/openwhisk/extension/whisk/OpenWhiskActionBuilder.scala
+++ tests/performance/gatling_tests/src/gatling/scala/org/apache/openwhisk/extension/whisk/OpenWhiskActionBuilder.scala
@@ -115,7 +115,7 @@ case class OpenWhiskActionBuilderWithAction(private val uuid: Expression[String]
 
   /** Invoke the action. */
   def invoke() = {
-    OpenWhiskActionBuilder(http.post(path).queryParam("blocking", "false").basicAuth(uuid, key))
+    OpenWhiskActionBuilder(http.get(path).queryParam("blocking", "false").basicAuth(uuid, key))
   }
 }
 
