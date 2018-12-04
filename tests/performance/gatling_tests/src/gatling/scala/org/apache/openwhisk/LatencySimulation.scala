f9809ef753cc0b83b45c889c488e7e182ca003a1
Victor Chen <yvictorck@gmail.com>
2018-12-04 09:42:46 -0800

    a

--- tests/performance/gatling_tests/src/gatling/scala/org/apache/openwhisk/LatencySimulation.scala
+++ tests/performance/gatling_tests/src/gatling/scala/org/apache/openwhisk/LatencySimulation.scala
@@ -90,7 +90,7 @@ class LatencySimulation extends Simulation {
           .action("${action._3}")
           .create(code, "${action._1}", "${action._4}"))
         .exec(openWhisk("Cold ${action._1} invocation").authenticate(uuid, key).action("${action._3}").invoke())
-        .exec { session => println(session); session }
+        // .exec { session => println(session); session }
         // .exec(openWhisk("Cold ${action._1} invocation response meow").authenticate(uuid, key).action("${action._3}").get())
         // .exec { session => println(session("responseBody").as[String]); session}
         .repeat(1) {
