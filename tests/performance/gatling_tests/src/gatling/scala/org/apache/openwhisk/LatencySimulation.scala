86099dee49538ad9b2e6d9d0b7f9f6720ec3b7d3
Victor Chen <yvictorck@gmail.com>
2018-12-04 09:20:40 -0800

    a

--- tests/performance/gatling_tests/src/gatling/scala/org/apache/openwhisk/LatencySimulation.scala
+++ tests/performance/gatling_tests/src/gatling/scala/org/apache/openwhisk/LatencySimulation.scala
@@ -90,10 +90,10 @@ class LatencySimulation extends Simulation {
           .action("${action._3}")
           .create(code, "${action._1}", "${action._4}"))
         .exec(openWhisk("Cold ${action._1} invocation").authenticate(uuid, key).action("${action._3}").invoke())
-        // .exec { session => println(session); session }
+        .exec { session => println(session); session }
         // .exec(openWhisk("Cold ${action._1} invocation response meow").authenticate(uuid, key).action("${action._3}").get())
         // .exec { session => println(session("responseBody").as[String]); session}
-        .repeat(10) {
+        .repeat(1) {
           // Add a pause of 100 milliseconds. Reason for this pause is, that collecting of logs runs asynchronously in
           // invoker. If this is not finished before the next request arrives, a new cold-start has to be done.
           pause(pauseBetweenInvokes.milliseconds)
