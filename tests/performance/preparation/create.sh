d475fd2cd4dec9c0cb62dedda74049766353813b
Victor Chen <yvictorck@gmail.com>
2018-12-04 23:52:08 -0800

    a

--- tests/performance/preparation/create.sh
+++ tests/performance/preparation/create.sh
@@ -26,8 +26,8 @@ action=$3
 
 # create a noop action
 echo "Creating action $action"
-curl -k -u "$credentials" "$host/api/v1/namespaces/_/actions/$action" -XPUT -d '{"namespace":"_","name":"test","exec":{"kind":"nodejs:default","code":"function main(){return {};}"}}' -H "Content-Type: application/json"
+curl -k -u "$credentials" "$host:443/api/v1/namespaces/_/actions/$action" -XPUT -d '{"namespace":"_","name":"test","exec":{"kind":"nodejs:default","code":"function main(){return {};}"}}' -H "Content-Type: application/json"
 
 # run the noop action
 echo "Running $action once to assert an intact system"
-curl -k -u "$credentials" "$host/api/v1/namespaces/_/actions/$action?blocking=true" -XPOST
+curl -k -u "$credentials" "$host:443/api/v1/namespaces/_/actions/$action?blocking=true" -XPOST
