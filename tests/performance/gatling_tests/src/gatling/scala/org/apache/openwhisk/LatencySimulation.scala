afef276b5af6f0beb7cdd081efcd65dd5c403795
Victor Chen <yvictorck@gmail.com>
2018-12-12 06:49:21 -0800

    a

--- tests/performance/gatling_tests/src/gatling/scala/org/apache/openwhisk/LatencySimulation.scala
+++ tests/performance/gatling_tests/src/gatling/scala/org/apache/openwhisk/LatencySimulation.scala
@@ -106,7 +106,7 @@ class LatencySimulation extends Simulation {
           // Add a pause of 100 milliseconds. Reason for this pause is, that collecting of logs runs asynchronously in
           // invoker. If this is not finished before the next request arrives, a new cold-start has to be done.
           // pause(pauseBetweenInvokes.milliseconds)
-            exec(openWhisk("med2 ${action._1} invocation").authenticate(uuid, key).action("${action._3}").invoke("7.5"))
+            exec(openWhisk("med2 ${action._1} invocation").authenticate(uuid, key).action("${action._3}").invoke("7"))
             // .exec { session => println(session); session }
 
         }
